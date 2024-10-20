e40b8ed66a3446777f36a378d3e81668959e691e
jaypatel3082001 <86408333+jaypatel3082001@users.noreply.github.com>
2024-10-20 13:16:42 +0530

    update1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className="bg-custom-dark text-[#FFC107] xl:py-10 flex justify-center max-[768px]:pt-8 max-[426px]:pt-0">
+    <footer className="bg-custom-dark text-[#FFC107] xl:py-10 flex justify-center max-[768px]:pt-8 max-[450px]:pt-0">
       <div className="flex xl:max-w-[1312px] max-xl:w-full max-xl:px-10 max-[600px]:px-4 justify-center max-md:pb-8">
         <div className="w-full mt-7 font-inter max-[700px]:flex max-[700px]:flex-col-reverse max-[700px]:mt-0">
           <div className='flex justify-center w-full'>
@@ -17,12 +17,12 @@ function Footer() {
             Book Silver Taxi Online
           </button> */}
               </div>
-              <div className="max-[426px]:flex hidden justify-center items-center max-[426px]:pb-2">
+              <div className="max-[450px]:flex hidden justify-center items-center max-[450px]:pb-2">
                 <span className="pr-2 text-sm max-[400px]:text-[10px]">Privacy Policy</span>
                 <span className="border-l-2 border-r-2 border-gray-400 px-2 text-sm max-[400px]:text-[10px]">Terms & Conditions</span>
                 <span className="pl-2 flex text-sm max-[400px]:text-[10px]">Collection Statement</span>
               </div>
-              <div className="max-[426px]:flex hidden justify-center items-center max-[426px]:pb-8">
+              <div className="max-[450px]:flex hidden justify-center items-center max-[450px]:pb-8">
               <span className="flex text-sm max-[400px]:text-[10px]">Sydney Taxi Service © 2024</span>
               </div>
               {/* Social Media Icons */}
@@ -50,7 +50,7 @@ function Footer() {
               <p>Sydney Taxi Service provides reliable, 24/7 transportation with a modern fleet to ensure your ride is smooth and hassle-free. Whether for local trips or airport transfers, book online or call us for top-quality service, anytime.</p>
             </div>
 
-            <div className='max-w-[30%] max-[426px]:hidden'>
+            <div className='max-w-[30%] max-[450px]:hidden'>
               <h3 className="text-xl font-semibold mb-3 underline">Useful Links</h3>
               <ul className="space-y-2">
 
@@ -68,7 +68,7 @@ function Footer() {
 
 
 
-            <div className='max-w-[30%] max-[426px]:hidden'>
+            <div className='max-w-[30%] max-[450px]:hidden'>
               <h3 className="text-xl font-semibold mb-3 underline">Contact Us</h3>
               <p>New South Wales, Australia</p>
               <p>Email: <a href="mailto:info@silverairporttaxis.com.au" className="hover:text-[#89bc21] break-words">info@silverairporttaxis.com.au</a></p>
